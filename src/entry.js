47c95603180234b0e11af318e401641e5613fe68
Mark Smith <mark.smith@practicalpoetry.co.uk>
2016-09-23 14:38:33 +0100

    Fixed the reload button bouncing up and down.

--- src/entry.js
+++ src/entry.js
@@ -1,34 +1,37 @@
-(function(){
-    require('./fittext.js');
-    var swear = require('./swear.js').swear;
+require('./fittext.js');
+var swear = require('./swear.js');
 
-    window.onload = function() {
-		var swearText = document.getElementById("swear"),
-			reloadButton = document.getElementById("reload_button");
-		var setSwear = function() {
-			var s = swear();
-			swearText.textContent = s;
-		};
+window.onload = function() {
+	var swearText = document.getElementById("swear"),
+		swearContent = swearText.getElementsByClassName("swearContent")[0],
+		reloadButton = document.getElementById("reload_button"),
+        mi = document.getElementById("more_info");
 
-		window.fitText(swearText);
-		reloadButton.onclick = function(evt) {
-			ga('send', 'event', 'reload', 'press')
-			setSwear();
-			// reloadButton.style.transform = "rotate(180deg)";
-			evt.preventDefault();
-			return false;
-		}
-		var mi = document.getElementById("more_info");
-		mi.onclick = function(evt) {
-			var info = document.getElementById("info");
-			if (info.getAttribute("hidden") !== null) {
-				info.removeAttribute("hidden");
-			} else {
-				info.setAttribute("hidden", "true");
-			}
-			evt.preventDefault();
-			return false;
-		};
-		setSwear();
+	var setSwear = function() {
+		var s = swear();
+		swearContent.textContent = s;
 	};
-})();
+
+    reloadButton.onclick = function(evt) {
+		ga('send', 'event', 'reload', 'press')
+		setSwear();
+		// reloadButton.style.transform = "rotate(180deg)";
+		evt.preventDefault();
+		return false;
+	}
+
+	mi.onclick = function(evt) {
+		var info = document.getElementById("info");
+		if (info.getAttribute("hidden") !== null) {
+			info.removeAttribute("hidden");
+		} else {
+			info.setAttribute("hidden", "true");
+		}
+		evt.preventDefault();
+		return false;
+	};
+
+    // Initialize view:
+	window.fitText(swearText);
+	setSwear();
+};
--- src/swear.js
+++ src/swear.js
@@ -1,131 +1,126 @@
-(function() {
-	var PRE_SWEAR = [
-		'arse',
-		'cock',
-		'crap',
-		'cunt',
-		'douche',
-		'fuck',
-		'piss',
-		'scrote',
-		'shit',
-		'shite',
-		'twat',
-		'twunt'
-	];
-	var POST_WORD = [
-		'badger',
-		'beetle',
-		'biscuit',
-		'boner',
-		'bubble',
-		'burger',
-		'cake',
-		'crumpet',
-		'curtain',
-		'divot',
-		'ferret',
-		'fiddle',
-		'fraggle',
-		'gibbon',
-		'lizard',
-		'mole',
-		'muffin',
-		'muppet',
-		'nugget',
-		'pilchard',
-		'pimple',
-		'prune',
-		'spanner',
-		'splat',
-		'spoon',
-		'toad',
-		'trumpet',
-		'tube',
-		'waffle',
-		'weasel',
-		'womble'
-	];
-	var POST_SWEAR = [
-		'crapper',
-		'felcher',
-		'fucker',
-		'pisser',
-		'shitter'
-	];
-	var PRE_WORD = [
-		'bunny',
-		'cabbage',
-		'cake',
-		'crab',
-		'crumpet',
-		'gibbon',
-		'hamster',
-		'lettuce',
-		'mole',
-		'muffin',
-		'muffin',
-		'muppet',
-		'slug',
-		'toad'
-	];
-	var POST_ADJECTIVE = [
-		'breathing',
-		'drinking',
-		'eating',
-		'eyed',
-		'fiddling',
-		'handed',
-		'headed',
-		'licking',
-		'mouthed',
-		'toed'
-	];
+var PRE_SWEAR = [
+	'arse',
+	'cock',
+	'crap',
+	'cunt',
+	'douche',
+	'fuck',
+	'piss',
+	'scrote',
+	'shit',
+	'shite',
+	'twat',
+	'twunt'
+];
+var POST_WORD = [
+	'badger',
+	'beetle',
+	'biscuit',
+	'boner',
+	'bubble',
+	'burger',
+	'cake',
+	'crumpet',
+	'curtain',
+	'divot',
+	'ferret',
+	'fiddle',
+	'fraggle',
+	'gibbon',
+	'lizard',
+	'mole',
+	'muffin',
+	'muppet',
+	'nugget',
+	'pilchard',
+	'pimple',
+	'prune',
+	'spanner',
+	'splat',
+	'spoon',
+	'toad',
+	'trumpet',
+	'tube',
+	'waffle',
+	'weasel',
+	'womble'
+];
+var POST_SWEAR = [
+	'crapper',
+	'felcher',
+	'fucker',
+	'pisser',
+	'shitter'
+];
+var PRE_WORD = [
+	'bunny',
+	'cabbage',
+	'cake',
+	'crab',
+	'crumpet',
+	'gibbon',
+	'hamster',
+	'lettuce',
+	'mole',
+	'muffin',
+	'muffin',
+	'muppet',
+	'slug',
+	'toad'
+];
+var POST_ADJECTIVE = [
+	'breathing',
+	'drinking',
+	'eating',
+	'eyed',
+	'fiddling',
+	'handed',
+	'headed',
+	'licking',
+	'mouthed',
+	'toed'
+];
 
 
-	var getRandomInt = function(min, max) {
-		return Math.floor(Math.random() * (max - min)) + min;
-	};
-	var randint = function(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
-	var choice = function(arr) {
-		return arr[getRandomInt(0, arr.length)]
-	};
-	var wordChooser = function(arr) {
-		return function() {
-			return choice(arr);
-		}
-	};
-	var preSwear = wordChooser(PRE_SWEAR),
-		postAdjective = wordChooser(POST_ADJECTIVE),
-		preWord = wordChooser(PRE_WORD),
-		postWord = wordChooser(POST_WORD),
-		postSwear = wordChooser(POST_SWEAR);
-	var adjective = function() {
-		//shit-breathing
-		return preSwear() + '-' + postAdjective();
-	};
-	var noun = function() {
-		if (randint(0, 2) < 2) {
-			// shitweasel
-			return preSwear() + postWord();
-		} else {
-			// muppetshitter
-			return preWord() + postSwear();
-		}
-	};
-
-	module.exports = {
-		swear: function() {
-			if (randint(0, 2) < 2) {
-				// shit-breathing muppetshitter
-				return adjective() + ' ' + noun();
-			} else {
-				// crabfucker
-				return noun();
-			}
-		}
+var getRandomInt = function(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+};
+var randint = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+var choice = function(arr) {
+	return arr[getRandomInt(0, arr.length)]
+};
+var wordChooser = function(arr) {
+	return function() {
+		return choice(arr);
 	}
+};
+var preSwear = wordChooser(PRE_SWEAR),
+	postAdjective = wordChooser(POST_ADJECTIVE),
+	preWord = wordChooser(PRE_WORD),
+	postWord = wordChooser(POST_WORD),
+	postSwear = wordChooser(POST_SWEAR);
+var adjective = function() {
+	//shit-breathing
+	return preSwear() + '-' + postAdjective();
+};
+var noun = function() {
+	if (randint(0, 2) < 2) {
+		// shitweasel
+		return preSwear() + postWord();
+	} else {
+		// muppetshitter
+		return preWord() + postSwear();
+	}
+};
 
-})();
+module.exports = function() {
+	if (randint(0, 2) < 2) {
+		// shit-breathing muppetshitter
+		return adjective() + ' ' + noun();
+	} else {
+		// crabfucker
+		return noun();
+	}
+};
